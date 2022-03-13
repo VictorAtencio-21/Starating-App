35805af0b46ec95afa21bc6e3d90e8c14e5e926d
Victor Atencio <65867307+VictorAtencio-21@users.noreply.github.com>
2022-03-13 16:48:43 -0400

    Added dark theme toggle

--- src/app/pages/tabnav/tabnav-routing.module.ts
+++ src/app/pages/tabnav/tabnav-routing.module.ts
@@ -21,6 +21,10 @@ const routes: Routes = [
         loadChildren: () => import('../profile/profile.module').then(m => m.ProfilePageModule)
       },
       {
+        path: 'settings',
+        loadChildren: () => import('../settings/settings.module').then(m => m.SettingsPageModule)
+      },
+      {
         path: '',
         redirectTo: '/tabs/dashboard',
         pathMatch: 'full'
